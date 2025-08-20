7694fe79a4134f11f1f83d6cc698aa7f97142302
viccelis <vm.celis29@ciaf.edu.co>
2025-08-19 21:24:50 -0500

    implementacion de las botones

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Contador</h1>
+      <div className='contenedor'>
+        
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            Mas 1
+          </button>
+        </div>
+
+        <div className="card">
+          <button onClick={() => setCount((count) => count - 1)}>
+            Menos 1
+          </button>
+        </div>
+
+        <div className="card">
+          <button onClick={() => setCount((count) => count = 0)}>
+            Reset
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id='Counter' className='Counter'>
+        Count is {count}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
